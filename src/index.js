8ea289fc58bf7e3860c1a8d991c0abbb434ff302
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-10-06 16:42:00 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
+const main = document.querySelector("#main-content")
+console.log(main)
+
+//select the form id to grap the submit event 
+const taskForm = document.querySelector("#create-task-form")
+
+//slap it on the document or to display onto the page. where ? into the tasks id 
+const taksList = document.querySelector("#tasks")
+
+//listn to the sumit event 
+taskForm.addEventListener("submit", function(e) {
+
+        //To grap whatever user enter in the input 
+        const newTask = document.querySelector("#new-task-description").value
+
+        //prevent z html event form submit to use the js form submit 
+        e.preventDefault()
+
+        //slap it on the document or to display onto the page where ? into the tasks id 
+        //do we have to wait untill the form submit happen to grap the task ? No
+        // or does the task exit on the page? Yes 
+        //should we have to grap it every time We hit submit?  no so we need to move 
+        //it to the top of the page so we will grap it out side 
+
+        taksList.innerHTML +=
+            `<li>${newTask}
+        <button data-action="delete"> X to delete </button>
+        </li>`
+
+        // const taskItem = document.createElement("li")
+        // taskItem.innerText = newTask
+        // taksList.appendChild(taskItem);
+
+        //clears user input 
+        taskForm.reset()
+    })
+    //the parent element know about z child so that we used 
+    // the event listner over here
+taksList.addEventListener("click", function(e) {
+    if (e.target.dataset.action == "delete") {
+        e.target.parentElement.remove()
+    }
+})
+
+
+/* other way to to solve the lab 
+
 document.addEventListener("DOMContentLoaded", () => {
     //grab all the necessary DOM elements
 
@@ -23,7 +70,4 @@ const createNewTask = event => {
     appendNewTask(newTask);
     event.target.reset();
 };
-
-const appendNewTask = task => {
-    document.getElementById("tasks").appendChild(task);
-};
+*/
